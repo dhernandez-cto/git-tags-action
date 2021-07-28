2c5710893b5cf604d780cededc208d6fc6d51076
David Hernández Martín <belcebus@gmail.com>
2021-07-28 22:25:20 +0200

    spliting

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const simpleGit = require('simple-git');
+const getReleaseCandidateNumber = require("./release-candidate.js");
 
-const git = simpleGit();
+async function run() {
 
-try {
     const versionToFilter = core.getInput('version');
-    console.log(`input version: ${versionToFilter}`);
-    const number = findTag(versionToFilter).then();
-    console.log(number);
-    core.setOutput("next-rc-number",number);
+    
+    try {
 
-} catch (error) {
-  core.setFailed(error.message);
-}
+        console.log(`input version: ${versionToFilter}`);
+        
+        const number = await getReleaseCandidateNumber(versionToFilter);
+        
+        console.log(number);
 
-async function findTag(version){
-    const tags = await git.tags();
-    console.log(tags.all);
-
-    const rcVersionTemplate=version+"-RC-";
-    console.log(rcVersionTemplate);
-
-    let re = new RegExp(rcVersionTemplate);
-
-    const tagsMatching = tags.all.filter(element => re.test(element));
-    console.log(tagsMatching.lenght+1);
-
-    return tagsMatching.lenght+1;
+        core.setOutput("next-rc-number",number);
+    
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 }
---
+++ release-candidate.js
@@ -0,0 +1,21 @@
+const simpleGit = require('simple-git');
+
+const git = simpleGit();
+
+async function getReleaseCandidateNumber(version){
+
+    const tags = await git.tags();
+    console.log(tags.all);
+
+    const rcVersionTemplate=version+"-RC-";
+    console.log(rcVersionTemplate);
+
+    let re = new RegExp(rcVersionTemplate);
+
+    const tagsMatching = tags.all.filter(element => re.test(element));
+    console.log(tagsMatching.lenght+1);
+
+    return tagsMatching.lenght+1;
+}
+
+module.exports = getReleaseCandidateNumber
