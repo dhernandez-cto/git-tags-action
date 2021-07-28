617f3152a25e18f3e0d808a5a42299a9ef8cf939
David Hernández Martín <belcebus@gmail.com>
2021-07-28 21:36:01 +0200

    implementing logic

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const simpleGit = require('simple-git');
+const git = simpleGit();
 
 try {
-  const versionToFilter = core.getInput('version');
-  console.log(`input version: ${versionToFilter}`);
-
-    /*
-        put the logic here
-    */
-
-  var lastReleaseCandidateNumber = 101;
-  core.setOutput("next-rc-number",lastReleaseCandidateNumber);
+    const versionToFilter = core.getInput('version');
+    console.log(`input version: ${versionToFilter}`);
+    
+    core.setOutput("next-rc-number",findTag(versionToFilter));
 
 } catch (error) {
   core.setFailed(error.message);
+}
+
+async function findTag(version){
+    const tags = await git.tags();
+    console.log("Taglist: " + tags);
+
+    const rcVersionTemplate=version+"-RC-";
+    console.log(rcVersionTemplate);
+
+    let re = new RegExp(rcVersionTemplate);
+
+    const tagsMatching = tags.all.filter(element => re.test(element));
+    return tagsMatching.lenght
 }
