67e8c27b43a30b73aed2c860b45c7f0660933a52
David Hernández Martín <belcebus@gmail.com>
2021-07-29 14:13:35 +0200

    testing

--- dist/index.js
+++ dist/index.js
@@ -6233,33 +6233,29 @@ exports.pick = pick;
 
 const simpleGit = __nccwpck_require__(1383);
 
+const tagRefPrefix = "refs/tags/";
 /*
     TODOS:
-        - implementar el testing de este método
-        - calcular en lugar de por número de elementos por el sufijo
-        - retornar además del número de tag, el tag completo
-        - verificar antes de retornar que no existe el tag
-        - recuperar los tags del remoto en lugar del local
+        - ( ) implementar el testing de este método
+        - ( ) calcular en lugar de por número de elementos por el sufijo
+        - ( ) retornar además del número de tag, el tag completo
+        - ( ) verificar antes de retornar que no existe el tag
+        - (X) recuperar los tags del remoto en lugar del local
 */
 
 async function getReleaseCandidateNumber(version){
     const git = simpleGit.default();
-    
-    const status = await git.status();
-    const branch = await git.branch();
-    
-    console.log(branch);
-    console.log(status);
 
-    const tags = await git.tags();
-    console.log(tags.all);
+    const remoteTagsArr = (await git.listRemote(['--tags','--quiet', '--refs'])).split(/\r?\n/).map(item => item.replace(/.*tags\//,""));
+
+    console.log(remoteTagsArr);
 
     const rcVersionTemplate=version;
-    console.log(rcVersionTemplate);
+    console.log('Filtering by' + rcVersionTemplate);
 
     let re = new RegExp(rcVersionTemplate);
 
-    const tagsMatching = tags.all.filter(element => re.test(element));
+    const tagsMatching = remoteTagsArr.filter(element => re.test(element));
     
     console.log(tagsMatching.length+1);
 
